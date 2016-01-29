d8896843132fdd1571d4a913e78c225b6a1603fe
Alex_Kersey <kerseyma@mail.uc.edu>
2016-01-29 13:34:33 -0500

    now creating GeoJSON object.

--- mapbox-android-sdk-legacy-mb-pages/MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/NavigationFragment.java
+++ mapbox-android-sdk-legacy-mb-pages/MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/NavigationFragment.java
@@ -1,5 +1,6 @@
 package com.mapbox.mapboxsdk.android.testapp;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,14 +9,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.os.AsyncTask;
 
 import org.json.JSONObject;
+import org.json.JSONException;
+
+
 import java.io.BufferedReader;
-import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by smith6s3 on 1/26/2016.
@@ -25,16 +30,12 @@ public class NavigationFragment extends Fragment {
 
     private Button searchButton;
     private EditText inputAddress;
-    private TextView testText;
-    //private JSONObject addressJson;
-    private HttpURLConnection httpURLConnection = null;
     private String addressUrl, search1, search2, token;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_navigation, container, false);
 
         inputAddress = (EditText) view.findViewById(R.id.InputAddress);
-        testText = (TextView) view.findViewById(R.id.testText);
         search1 = String.format(getResources().getString(R.string.search_query_1));
         search2 = String.format(getResources().getString(R.string.search_query_2));
         token = String.format(getResources().getString(R.string.testAccessToken));
@@ -43,26 +44,9 @@ public class NavigationFragment extends Fragment {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                URL url = null;
-                try {
-                    url = new URL(BuildSearchString(inputAddress.getText().toString()));
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } finally {
-                    httpURLConnection = (HttpURLConnection) url.openConnection();
-                    try {
-                        InputStream in = new BufferedInputStream(httpURLConnection.getInputStream());
-                        InputStream in = address.openStream();
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                        StringBuilder result = new StringBuilder();
-                        String line;
-                        while((line = reader.readLine()) != null) {
-                            result.append(line);
-                        }
-                    } finally {
-                        httpURLConnection.disconnect();
-                    }
-                }
+                addressUrl = BuildSearchString(inputAddress.getText().toString());
+                DownloadGeoJsonFile downloadGeoJsonFile = new DownloadGeoJsonFile();
+                downloadGeoJsonFile.execute(addressUrl);
             }
         });
 
@@ -80,6 +64,44 @@ public class NavigationFragment extends Fragment {
 
 
     //AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+    private class DownloadGeoJsonFile extends AsyncTask<String, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            try {
+                // Open a stream from the URL
+                InputStream stream = new URL(params[0]).openStream();
+
+                String line;
+                StringBuilder result = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+                while ((line = reader.readLine()) != null) {
+                    // Read and save each line of the stream
+                    result.append(line);
+                }
+
+                // Close the stream
+                reader.close();
+                stream.close();
+
+                // Convert result to JSONObject
+                return new JSONObject(result.toString());
+            } catch (IOException e) {
+            } catch (JSONException e) {
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            if (jsonObject != null) {
+            }
+        }
+
+
+    }
 
 
 }
